04e3e4b96be8ba9a0fa76364eaf414e647159e0d
Marios Daoutis <mds@monads.io>
2015-02-10 00:10:00 +0100

    testing package

--- Makefile
+++ Makefile
@@ -12,12 +12,9 @@ include $(XSH_HOME)/Makefile.in
 
 
 
-
-
-
-
-
 all: detect-julia
 	# @echo $(IS_GIT)
 	# @echo $(JULIA) 
 	# @echo $(USE_SYSTEM_JULIA) 
+	# @echo $(XSH_VER) 
+	# @echo $(XSH_COMMIT) 
--- src/xsh.jl
+++ src/xsh.jl
@@ -1,5 +1,19 @@
 module xsh
-
+using JSON
+using UUID
 # package code goes here
+println(UUID.v4())
 
+# JSON.parse - string or stream to Julia data structures
+s = "{\"a_number\" : 5.0, \"an_array\" : [\"string\", 9]}"
+j = JSON.parse(s)
+#  Dict{String,Any} with 2 entries:
+#    "an_array" => {"string",9}
+#    "a_number" => 5.0
+println(j)
+# JSON.json - Julia data structures to a string
+println(JSON.json([2,3]))
+#  "[2,3]"
+println(JSON.json(j))
+#  "{\"an_array\":[\"string\",9],\"a_number\":5.0}"
 end # module
