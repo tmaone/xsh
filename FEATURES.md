7e53eaf6303ab99cca5c3b2ae0bbb317af8d6f5a
Marios Daoutis <mds@monads.io>
2015-02-10 13:49:56 +0100

    initials

---
+++ FEATURES.md
@@ -0,0 +1,17 @@
+# xsh Features Roadmap v1.0 
+
+## Major Features
+
+* Integration with modern (widely used) Terminal Emulators (iTerm2 mostly)
+    - Display image files
+    - Display documents
+    - Marks
+    - Alert when ps/fn/cmd finishes
+    - Drag/Drop Files, Local, Remote
+    - Switch Profiles
+
+* Color Profiles in xshrc
+
+## Minor Features
+
+* Automated Version files/headers generation from Makefile/Git
--- src/xsh.jl
+++ src/xsh.jl
@@ -1,45 +1,91 @@
-module xsh
-using JSON
-using UUID
-# package code goes here
-println(UUID.v4())
-
-# JSON.parse - string or stream to Julia data structures
-s = "{\"a_number\" : 5.0, \"an_array\" : [\"string\", 9]}"
-j = JSON.parse(s)
-#  Dict{String,Any} with 2 entries:
-#    "an_array" => {"string",9}
-#    "a_number" => 5.0
-println(j)
-# JSON.json - Julia data structures to a string
-println(JSON.json([2,3]))
-#  "[2,3]"
-println(JSON.json(j))
-#  "{\"an_array\":[\"string\",9],\"a_number\":5.0}"
 
 
 
-function _start(ARGS)
-    println("xsh>")
-    Base.versioninfo()
-    function input(prompt::String="")
-         print(prompt)
-         chomp(readline(STDIN))
-    end
-    for a in ARGS
-        println(a)
-    end
-    # while true
-        # inputs = input(">");
-        # println(inputs)
-    # end
-end
 
-function test(in::String)
-    println("xsh> ", "$(in)")
-end
 
-end # module
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# module xsh
+# using JSON
+# using UUID
+# # package code goes here
+# println(UUID.v4())
+
+# # JSON.parse - string or stream to Julia data structures
+# s = "{\"a_number\" : 5.0, \"an_array\" : [\"string\", 9]}"
+# j = JSON.parse(s)
+# #  Dict{String,Any} with 2 entries:
+# #    "an_array" => {"string",9}
+# #    "a_number" => 5.0
+# println(j)
+# # JSON.json - Julia data structures to a string
+# println(JSON.json([2,3]))
+# #  "[2,3]"
+# println(JSON.json(j))
+# #  "{\"an_array\":[\"string\",9],\"a_number\":5.0}"
+# msg = "xsh"string(char(0xb83))" "
+# println(msg)
+
+
+# function _start(ARGS)
+#     println(msg)
+#     Base.versioninfo()
+#     function input(prompt::String="")
+#          print(prompt)
+#          chomp(readline(STDIN))
+#     end
+#     for a in ARGS
+#         println(a)
+#     end
+#     # while true
+#         # inputs = input(">");
+#         # println(inputs)
+#     # end
+# end
+
+# function test(in::String)
+#     println(msg, "$(in)")
+# end
+
+# end # module
 
 
 
